2142d2745e1a6050d1041c82ea106476a12bc05c
Tausani Ah Chong <tausani93@gmail.com>
2021-01-21 11:47:43 +1300

    iniial server setup, show home

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+const routes = require('./routes')
+
+const server = express()
+
+module.exports = server
+
+server.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+
+server.set('view engine', 'hbs')
+
+server.use('/', routes)
